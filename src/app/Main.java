8d141710a33d8e857da8c66c246c7240d421ec8b
Gergely Takacs <takgerg@yahoo.com>
2023-08-21 02:30:07 +0200

    Cleaned a little

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,10 +1,8 @@
 package app;
 
 
-import com.sun.javafx.application.LauncherImpl;
 import controller.TestCenterController;
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import testcenterfx.LauncherPreloader;
 import testcenterfx.PreloaderFXMLController;
@@ -15,13 +13,9 @@ import testcenterfx.PreloaderFXMLController;
  */
 public class Main extends Application {
     
-    //static Logger logger = LoggerFactory.getLogger(Main.class);
-
     public static TestCenterController controller = new TestCenterController();
     
     public static Stage primaryStage = null;
-    public static Scene primaryScene = null;
-
     @Override
     public void init() throws Exception {        
         PreloaderFXMLController init = new PreloaderFXMLController();
@@ -34,7 +28,6 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        //LauncherImpl.launchApplication(Main.class, LauncherPreloader.class, args);
         System.setProperty("javafx.preloader", LauncherPreloader.class.getCanonicalName());
         Application.launch(Main.class, args);
     }
